7a701541f4dfa299790b5bf52cf77aa576ee3078
vibrantlife <nicholesvibrantlife@gmail.com>
2015-03-10 13:44:56 -0700

    animation for signin is working

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
   $(function(){
-    var $(email) = $("")
-  })
+    var $email = $('#email');
+    var $pass = $('password');
+    var $button = $('.index_main_body input[type=submit]');
 
+    $button.click(function(){
+      var e = $email.val();
+      var p = $pass.val();
+      if (e && p) //only testing they've been entered
+      {
+        $email.removeClass('invalid');
+        $pass.removeClass('invalid');
+      } else {
+        $email.addClass('invalid');
+        $pass.addClass('invalid');
+      }
+    });
+  });
 });
